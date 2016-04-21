0b3ea41bb74f6f0d1a9533eb5b3428b8057dc4a0
Ashwin Rao <theonlyrao@gmail.com>
2016-04-21 14:48:30 -0600

    set current user in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,11 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
+    if session[:user_id].nil?
+      @current_user = nil
+    else
+      @current_user = User.find(session[:user_id])
+    end
 
-    @current_user = nil || User.find(session[:user_id])
   end
 end
