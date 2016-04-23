d36eb94a6a22ffdddca335931385cfab9e0875f7
Christopher Soden <karavol27@gmail.com>
2016-04-23 13:24:22 -0600

    Add total cost method to the Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,11 @@
 class Order < ActiveRecord::Base
   has_many :order_tools
 
-
   enum status: ["completed", "cancelled", "ordered", "paid"]
+
+  def total
+    order_tools.reduce(0) do |sum, line_item|
+      sum += line_item.subtotal
+    end
+  end
 end
