13e4a7af17070736999a9c7cdba6c90e8f17032b
Christopher Soden <karavol27@gmail.com>
2016-04-23 13:14:25 -0600

    Add status enum to the Order table

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ActiveRecord::Base
   has_many :order_tools
+
+
+  enum status: ["completed", "cancelled", "ordered", "paid"]
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   it {should have_many :order_tools}
+
+  it "has status completed, cancelled, ordered, paid" do
+    assert_equal Order.statuses, {"completed" => 0, "cancelled" => 1, "ordered" => 2, "paid" => 3 }
+  end
 end
