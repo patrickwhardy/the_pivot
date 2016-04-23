8553aa79b0b44e70ceed97821217df9f4a5aecef
Christopher Soden <karavol27@gmail.com>
2016-04-23 13:38:25 -0600

    Implement User can view a specific past order
    User can view a past order. Each tool name in the order page is a link
    that will take the user to a show page with details about that tool.

--- spec/features/user_can_view_a_past_order_spec.rb
+++ spec/features/user_can_view_a_past_order_spec.rb
@@ -43,6 +43,8 @@ RSpec.feature "User can view a past order" do
     #   And I should see links to each item's show page
     click_on "#{order_tool2.tool.name}"
     assert_equal tools_path(order_tool2.tool.id),current_path
+    expect(page).to have_content "#{tool2.name}"
+    expect(page).to have_content "#{tool2.description}"
 
   end
 end
