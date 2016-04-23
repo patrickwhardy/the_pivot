fbefecb7c66ffe0c790325882d9e8e4d44198d53
Christopher Soden <karavol27@gmail.com>
2016-04-23 17:54:04 -0600

    Implement admin item creation

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,2 +1,10 @@
 class Admin::BaseController < ApplicationController
+  before_action :check_for_admin
+
+  def check_for_admin
+    # byebug
+    unless session[:user_id] && current_user.admin?
+      render file: "#{Rails.root}/public/404.html", status: 404
+    end
+  end
 end
