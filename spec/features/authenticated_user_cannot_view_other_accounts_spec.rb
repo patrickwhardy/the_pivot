130ba42e1553d6dec6cec11469432c14a8c36b6e
Christopher Soden <karavol27@gmail.com>
2016-04-23 16:18:57 -0600

    Test that a user can see their dashboard and no one elses

---
+++ spec/features/authenticated_user_cannot_view_other_accounts_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.feature "Authenticated logs in" do
+  include SpecTestHelper
+
+  scenario "cannot view other accounts" do
+    user1 = create(:user, username: "Tom")
+    user2 = create(:user, username: "Susan")
+    # Background: An authenticated user and the ability to add an admin user
+    #   As an Authenticated User
+
+    login_user(user1)
+    #   I cannot view another user's private data (current or past orders, etc)
+
+    visit dashboard_path(user2.id)
+    expect(page).to have_content user1.username
+    expect(page).to have_no_content user2.username
+    #   I cannot view the administrator screens or use admin functionality
+
+    #   I cannot make myself an admin
+
+
+
+
+  end
+end
