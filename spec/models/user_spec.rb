3ecd2f2dc441c9f7ae72fc7eac1e165f55c4504f
Christopher Soden <karavol27@gmail.com>
2016-04-23 14:53:13 -0600

    Add admin/user role to the user table. Test that default is user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+    it "can assign an admin" do
+      admin = create(:user, role: 1)
+      assert_equal "admin", admin.role
+    end
+
+    it "defaults to a user" do
+      user = create(:user)
+      assert_equal "user", user.role
+    end
 end
