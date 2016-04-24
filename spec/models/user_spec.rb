429a659a2c449e08a0696bf1d1a599c1b131a11f
Ashwin Rao <theonlyrao@gmail.com>
2016-04-23 23:35:55 -0600

    remove view orders link for users without orders

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe User, type: :model do
       user = User.new(password: "password")
       refute user.save
     end
+
+    it {should have_many :orders}
+
+    it "knows if user has orders" do
+      user = create(:user)
+      Order.create(user_id: user.id)
+
+      assert_equal 1, user.orders.count
+      assert user.has_orders?
+    end
 end
