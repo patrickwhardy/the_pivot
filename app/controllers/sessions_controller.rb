b7018568ff3a43422514b631b376d9e4c533089e
Ashwin Rao <theonlyrao@gmail.com>
2016-04-26 19:45:47 -0600

    Remove cart_login method from SessionsController
    Previously we had created a login view specifically for users who logged in
    from the cart view.
    
    This commit refactors the SessionsController so that now all users use the
    standard `new.html.erb` view through the `new` method by using a new instance
    variable that is true if the referring path is "/cart/login", thus triggering
    a hidden field in the view.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,12 @@ class SessionsController < ApplicationController
 
   def new
     @user = User.new
+    @checkout = true if env["PATH_INFO"] == "/cart/login"
   end
 
   def destroy
     session.clear
     redirect_to root_path
-  end
-
-  def cart_login
-    @user = User.new
   end
 
   def create
