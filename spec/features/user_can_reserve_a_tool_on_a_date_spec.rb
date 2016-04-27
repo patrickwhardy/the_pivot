af4a64cf11bdc62406d8cf0a07b193d3dbd5f30e
Christopher Soden <karavol27@gmail.com>
2016-04-27 10:10:39 -0600

    Create test for initial reservation date use case

---
+++ spec/features/user_can_reserve_a_tool_on_a_date_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+RSpec.feature "User can reserve a tool on a date" do
+  include SpecTestHelper
+
+  scenario "- user views a tool and chooses a single date" do
+    # As a user,
+    user1 = create(:user)
+    tool1 = create(:tool)
+    tool2 = create(:tool)
+    login_user(user1)
+    date = "05/05/2016"
+    # when I view a tool,
+    visit tool_path(tool1.id)
+    fill_in "Reservation Date", with: date
+    click_on "Add to Cart"
+    click_on "Item"
+    click_on "Checkout"
+
+    assert_equal 1, Reservation.count
+    assert_equal tool1.id, Reservation.last.tool_id
+    assert_equal 1, Date.count
+    assert_equal date, Date.last.dates_reserved
+    assert_equal date.id, Reservation.last.date_id
+    assert_equal user.id, Reservation.last.user_id
+
+    click_on "Logout"
+
+    visit tool_path(tool1.id)
+    fill_in "Reservation Date", with: date
+    click_on "Add to Cart"
+    expect(page).to have_content "Tool Unavailable"
+
+    visit tool_path(tool2.id)
+    fill_in "Reservation Date", with: date
+    click_on "Add to Cart"
+    click_on "Item"
+    expect(page).to have_content "Login or Create Account to Checkout"
+    # I can choose a single date,
+    # and if the tool has already been reserved on that date I will see "tool unavailable,"
+    # and if the tool has not been reserved I can add the tool to my cart.
+  end
+end
