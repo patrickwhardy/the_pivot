d9bada0afb5b345d1151fda72d93c9e2f941e472
Josh Washke <jwashke@icloud.com>
2016-05-10 13:51:52 -0600

    Add description to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510194427) do
+ActiveRecord::Schema.define(version: 20160510195108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20160510194427) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
+    t.string   "description"
   end
 
   add_foreign_key "order_tools", "orders"
