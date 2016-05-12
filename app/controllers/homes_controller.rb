48a3d12638dead62c2c16185aaea52c64772538b
Ashwin Rao <theonlyrao@gmail.com>
2016-05-11 22:23:03 -0600

    Allow searching by zip code
    This commit creates search functionality for homes by home description. The
    tested search is by zip code, but this implementation would work for any search
    of homes's descriptions.

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,13 @@
 class HomesController < ApplicationController
   def index
+    if request.referrer == "http://www.example.com/search"
+      @homes = Home.where("description LIKE ?", "%#{params[:home][:description]}%").all
+    else
+      @homes = Home.all
+    end
+  end
+
+  def search
+    @home = Home.new
   end
 end
