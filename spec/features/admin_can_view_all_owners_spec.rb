4dc471189721258340dea59a2c52a1f1a41ecc8d
Ashwin Rao <theonlyrao@gmail.com>
2016-05-12 23:09:55 -0600

    let admin see list of all home owners

---
+++ spec/features/admin_can_view_all_owners_spec.rb
@@ -0,0 +1,59 @@
+require "rails_helper"
+
+RSpec.feature "Admin is logged in" do
+  scenario "goes to view all owners" do
+# As a logged-in admin,
+# when I am on my dashboard,
+# and I click on "view all owners,"
+# I can see a page that lists all the owners,
+# and for each owner,
+# I can see a link to that owner's show page,
+# see a link to that owner's dashboard,
+# see a link to edit that owner,
+# see a link to suspend that owner,
+# and see a link to delete that owner.
+    admin_user = create(:user)
+    admin_role = create(:role, role: "admin")
+    UserRole.create(user: admin_user, role: admin_role)
+    ApplicationController.any_instance.stubs(:current_user).returns(admin_user)
+    home_one = create(:home, name: "home 1")
+    home_two = create(:home, name: "home 2")
+    home_three = create(:home, name: "home 3", user: home_one.user)
+    homeless_user = create(:user)
+    
+    visit dashboard_path(admin_user.slug)
+    click_on "View All Owners"
+
+    expect(current_path).to eq(admin_owners_path)
+    within(".owner-#{home_one.user.id}") do
+      expect(page).to have_content(home_one.user.username)
+      expect(page).to have_link("User Dashboard", href: dashboard_path(home_one.user.slug))
+      expect(page).to have_link("User Homes", href: user_homes_path(home_one.user.slug))
+      expect(page).to have_content("Edit")
+      expect(page).to have_content("Suspend")
+      expect(page).to have_content("Delete")
+    end
+    within(".owner-#{home_two.user.id}") do
+      expect(page).to have_content(home_two.user.username)
+      expect(page).to have_link("User Dashboard", href: dashboard_path(home_two.user.slug))
+      expect(page).to have_link("User Homes", href: user_homes_path(home_two.user.slug))
+      expect(page).to have_content("Edit")
+      expect(page).to have_content("Suspend")
+      expect(page).to have_content("Delete")
+    end
+    expect(page).to have_no_content(homeless_user.username)
+  end
+
+  scenario "regular user is logged in" do
+    admin_user = create(:user, first_name: "admin")
+    admin_role = create(:role, role: "admin")
+    UserRole.create(user: admin_user, role: admin_role)
+    regular_user = create(:user, first_name: "regular")
+    regular_role = create(:role, role: "user")
+    UserRole.create(user: regular_user, role: regular_role)
+    ApplicationController.any_instance.stubs(:current_user).returns(regular_user)
+
+    visit admin_owners_path
+    expect(page).to have_content("404")
+  end
+end
