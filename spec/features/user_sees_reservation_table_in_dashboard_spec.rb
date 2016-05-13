d9e1bbd1d96f0e7d94a7a286fc2fcf2f32b7f8a0
patrickwhardy <patrickwhardy@gmail.com>
2016-05-12 21:38:32 -0600

    add homes upcoming and past reservations table

--- spec/features/user_sees_reservation_table_in_dashboard_spec.rb
+++ spec/features/user_sees_reservation_table_in_dashboard_spec.rb
@@ -6,7 +6,7 @@
 require "rails_helper"
 
 RSpec.feature "User sees reservation tables on dashboard" do
-  it "user with reservations" do
+  it "shows the user reservations" do
     order = create(:order)
     user = order.user
     home = create(:home)
@@ -34,6 +34,41 @@ RSpec.feature "User sees reservation tables on dashboard" do
     end
 
     within ".past-reservations" do
+      expect(page).to have_content(order.reservations.last.checkin)
+      expect(page).to have_content(order.reservations.last.checkout)
+      expect(page).not_to have_content(order.reservations.first.checkin)
+      expect(page).not_to have_content(order.reservations.first.checkout)
+    end
+  end
+
+  it "shows owner their home's reservations" do
+    home = create(:home)
+    owner = home.user
+    order = create(:order)
+
+    order.reservations << Reservation.new(
+          checkin: Date.parse("2017-05-22"),
+          checkout: Date.parse("2017-05-25"),
+          home: home
+    )
+
+    order.reservations << Reservation.new(
+          checkin: Date.parse("2015-05-22"),
+          checkout: Date.parse("2015-05-25"),
+          home: home
+    )
+
+    ApplicationController.any_instance.stubs(:current_user).returns(owner)
+    visit dashboard_path(owner)
+
+    within ".homes-upcoming-reservations" do
+      expect(page).to have_content(order.reservations.first.checkin)
+      expect(page).to have_content(order.reservations.first.checkout)
+      expect(page).not_to have_content(order.reservations.last.checkin)
+      expect(page).not_to have_content(order.reservations.last.checkout)
+    end
+
+    within ".homes-past-reservations" do
       expect(page).to have_content(order.reservations.last.checkin)
       expect(page).to have_content(order.reservations.last.checkout)
       expect(page).not_to have_content(order.reservations.first.checkin)
