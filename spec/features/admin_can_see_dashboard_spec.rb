7a8c7ec56732785907168a9b4f949a1c5c760289
Ashwin Rao <theonlyrao@gmail.com>
2016-05-12 21:05:45 -0600

    Confirm that regular cannot see admin's dashboard

--- spec/features/admin_can_see_dashboard_spec.rb
+++ spec/features/admin_can_see_dashboard_spec.rb
@@ -16,4 +16,19 @@ RSpec.feature "Admin can see dashboard" do
     expect(page).to have_content("View All Owners")
     expect(page).to have_content("View All Homes")    
   end
+
+  scenario "non-admin user logged in" do
+    admin_user = create(:user, first_name: "admin")
+    admin_role = create(:role, role: "admin")
+    UserRole.create(user: admin_user, role: admin_role)
+    regular_user = create(:user, first_name: "regular")
+    regular_role = create(:role, role: "user")
+    UserRole.create(user: regular_user, role: regular_role)
+    ApplicationController.any_instance.stubs(:current_user).returns(regular_user)
+    visit dashboard_path(admin_user.slug)
+
+    expect(page).to have_no_content("Welcome, #{admin_user.first_name}")
+    expect(page).to have_no_content("View All Owners")
+    expect(page).to have_content("Welcome, #{regular_user.first_name}")
+  end
 end
---
+++ spec/features/admin_can_view_all_homes_spec.rb
@@ -0,0 +1,6 @@
+require "rails_helper"
+
+RSpec.feature "Admin can view all homes" do
+  describe "admin is logged in" do
+  end
+end
