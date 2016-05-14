e39be1d54465688efb253490b66ce30a47df0d2a
patrickwhardy <patrickwhardy@gmail.com>
2016-05-13 18:37:15 -0600

    finish user profile feature

--- spec/features/user_can_view_profiles_spec.rb
+++ spec/features/user_can_view_profiles_spec.rb
@@ -16,14 +16,15 @@ RSpec.feature "user can view profiles" do
     ApplicationController.any_instance.stubs(:current_user).returns(user)
     visit dashboard_path(user)
     click_on("View All Homes")
+
     expect(current_path).to eq(user_homes_path(user.slug))
     expect(page).to have_content(user.first_name)
     expect(page).to have_content(user.last_name)
-    expect(page).to have_content(user.avatar)
+    expect(page).to have_xpath("//img[@src='#{user.avatar}']")
     expect(page).to have_content(user.description)
 
     user.homes.each do |home|
-      expect(page).to have_content(home.photos.first.image.url)
+      expect(page).to have_xpath("//img[@src='#{home.photos.first.image.url}']")
       expect(page).to have_content(home.name)
       expect(page).to have_content(home.description)
     end
