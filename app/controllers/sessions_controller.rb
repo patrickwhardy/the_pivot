89f567c219d166a9035a7d439421a8fb74b2a7ce
Josh Washke <jwashke@icloud.com>
2016-05-14 16:31:37 -0600

    Small Fix to sessions controller user account must be active to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:session][:username])
-    if @user && @user.authenticate(params[:session][:password])
+    if @user && @user.authenticate(params[:session][:password]) && @user.active?
       session[:user_id] = @user.id
       if @user.admin?
         flash[:success] = "Admin Login successful."
