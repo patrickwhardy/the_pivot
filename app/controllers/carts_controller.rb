6c25acf2d8db92b717ff016b292197ebace5f381
patrickwhardy <patrickwhardy@gmail.com>
2016-05-16 23:09:50 -0600

    reformat date in carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,20 @@
 class CartsController < ApplicationController
   def create
+    formatted_checkin = Hash.new
+    checkin_array = params[:date][:checkin_date].split("/")
+      3.times do |num|
+        formatted_checkin["date(#{num + 1}i)"] = checkin_array[num]
+      end
+    formatted_checkout = Hash.new
+    checkout_array = params[:date][:checkout_date].split("/")
+      3.times do |num|
+        formatted_checkout["date(#{num + 1}i)"] = checkout_array[num]
+      end
     reserved_dates = Reservation.dates_reserved(
-      params[:id], params[:checkin_date], params[:checkout_date]
+      params[:id], formatted_checkin, formatted_checkout
     )
     if reserved_dates.empty?
-      @cart.add_home(params[:id], params[:checkin_date], params[:checkout_date])
+      @cart.add_home(params[:id], formatted_checkin, formatted_checkout)
       session[:cart] = @cart.contents
       flash[:success] = "You've added this reservation to your cart"
     else
