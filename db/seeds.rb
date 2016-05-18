4bc0cda7e4578bb4a826cfeb7d94824f48d28555
Josh Washke <jwashke@icloud.com>
2016-05-18 13:17:22 -0600

    Update Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@ class Seed
   end
 
   def create_demo_data
+    puts "Creating Demo Data"
     # 8 homes in Denver in different neighborhoods
     # 3 owners owning each of these homes with full pictures and things
     # one reservation for each one in May or June
@@ -18,30 +19,143 @@ class Seed
 
     #shopping list: 24 tiny home picutres, 5 profile pictures
     josh = User.new(
-      email: "josh@example.com",
+      email: "jwashke@gmail.com",
       password: "password",
       username: "josh_turing",
       first_name: "josh",
       last_name: "lupercal",
-      description: "Member of the pivot team."
+      description: "I have the best tiny homes, You wont find better than these tiny homes."
     )
     josh.slug = josh.username.parameterize
     josh_home_one = Home.new(
-      name: generate_name,
+      name: "One bedroom Tiny Home with a view",
       description: generate_description,
       price_per_night: Faker::Number.number(2),
-      address: @addresses.pop
+      address: "1510 Blake Street Denver CO 80202"
     )
-    home.photos << Photo.create(
-      image: File.open(File.join(Rails.root, '/public/photos/tinyhome1.jpg'))
+    josh_home_one.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
     )
-    home.photos << Photo.create(
-      image: File.open(File.join(Rails.root, '/public/photos/tinyhome2.jpg'))
+    josh_home_one.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
     )
-    user.homes << home
-    puts users.save
+    josh_home_two = Home.new(
+      name: "Affordable pest friendly Tiny Home",
+      description: generate_description,
+      price_per_night: Faker::Number.number(2),
+      address: "8801 W Belleview Ave Littleton CO 80123"
+    )
+    josh_home_two.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
+    )
+    josh_home_two.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
+    )
+    josh.homes << josh_home_one
+    josh.homes << josh_home_two
+    puts josh.save
+
+    ashwin = User.new(
+      email: "ashwin@example.com",
+      password: "password",
+      username: "ashwin_turing",
+      first_name: "ashwin",
+      last_name: "lupercal",
+      description: "These are some pretty good tiny homes right here"
+    )
+    ashwin.slug = ashwin.username.parameterize
+    ashwin_home_one = Home.new(
+      name: "One bedroom Tiny Home with a view",
+      description: generate_description,
+      price_per_night: Faker::Number.number(2),
+      address: "4370 Quay St Wheat Ridge, CO 80033"
+    )
+    ashwin_home_one.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
+    )
+    ashwin_home_one.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
+    )
+    ashwin_home_two = Home.new(
+      name: "Affordable pest friendly Tiny Home",
+      description: generate_description,
+      price_per_night: Faker::Number.number(2),
+      address: "2960 Inca St Unit 107, Denver, CO 80202"
+    )
+    ashwin_home_two.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
+    )
+    ashwin_home_two.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
+    )
+    ashwin_home_three = Home.new(
+      name: "One bedroom Tiny Home with a view",
+      description: generate_description,
+      price_per_night: Faker::Number.number(2),
+      address: "106 S University Blvd Unit 13, Denver, CO 80209"
+    )
+    ashwin_home_three.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
+    )
+    ashwin_home_three.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
+    )
+    ashwin.homes << ashwin_home_one
+    ashwin.homes << ashwin_home_two
+    ashwin.homes << ashwin_home_three
+    puts ashwin.save
+
+    patrick = User.new(
+      email: "patrick@example.com",
+      password: "password",
+      username: "patrick_turing",
+      first_name: "patrick",
+      last_name: "lupercal",
+      description: "These are legally Tiny Homes, come and stay in them."
+    )
+    patrick.slug = ashwin.username.parameterize
+    patrick_home_one = Home.new(
+      name: "One bedroom Tiny Home with a view",
+      description: generate_description,
+      price_per_night: Faker::Number.number(2),
+      address: "4305 Zuni St, Denver, CO 80211"
+    )
+    patrick_home_one.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
+    )
+    patrick_home_one.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
+    )
+    patrick_home_two = Home.new(
+      name: "Affordable pest friendly Tiny Home",
+      description: generate_description,
+      price_per_night: Faker::Number.number(2),
+      address: "10718 Troy St, Commerce City, CO 80022"
+    )
+    patrick_home_two.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
+    )
+    patrick_home_two.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
+    )
+    patrick_home_three = Home.new(
+      name: "Affordable pest friendly Tiny Home",
+      description: generate_description,
+      price_per_night: Faker::Number.number(2),
+      address: "18062 E 104th Pl Unit C, Commerce City, CO 80022"
+    )
+    patrick_home_three.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
+    )
+    patrick_home_three.photos << Photo.create(
+      image: File.open(File.join(Rails.root, ""))
+    )
+    patrick.homes << patrick_home_one
+    patrick.homes << patrick_home_two
+    patrick.homes << patrick_home_three
+    puts patrick.save
   end
-  
+
   def create_admins
     puts "Creating Required Users"
     user = User.new(
