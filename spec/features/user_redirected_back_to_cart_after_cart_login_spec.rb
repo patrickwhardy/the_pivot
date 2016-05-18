bd48178d7f7ff99c658b9476a1fc77060d432737
Ashwin Rao <theonlyrao@gmail.com>
2016-05-18 14:40:05 -0600

    Redirect user back to cart after login if items in cart

---
+++ spec/features/user_redirected_back_to_cart_after_cart_login_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.feature "user redirected back to cart" do
+  scenario "user logs from cart checkout page" do
+    user = create(:user)
+    home = create(:home)
+    cart_contents = {"home" => home.id, "checkin" => Date.parse("2016-07-01"), "checkout" => Date.parse("2016-07-22")}
+    visit user_home_path(home.user.slug, home)
+
+    fill_in "date_checkin_date", with: "05/02/2017"
+    fill_in "date_checkout_date", with: "05/06/2017"
+
+    click_on("Add to Cart")
+    click_on("1 Reservation - $400.00")
+    within (".login-btn") do
+      click_link("Login")
+    end
+
+    fill_in "Password", with: "#{user.password}"
+    fill_in "Username", with: "#{user.username}"
+    click_button "Login"
+    
+    expect(current_path).to eq(cart_path)    
+  end
+end
