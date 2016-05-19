6bdab9ca2ad754e79314bd16294cfb70b734c3b0
Ashwin Rao <theonlyrao@gmail.com>
2016-05-19 00:26:59 -0600

    precompile for heroku

--- app/assets/javascripts/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  handler = Gmaps.build('Google');
-  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-    markers = handler.addMarkers(<%=raw @hash.to_json %>);
-    handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
-  });
-});
