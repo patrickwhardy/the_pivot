52986750bb14905e97ce5d6368d42c39c9846dec
Ashwin Rao <theonlyrao@gmail.com>
2016-05-18 23:09:20 -0600

    Reject reservation requests missing checkin or checkout dates

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,17 +5,21 @@ class CartsController < ApplicationController
     session[:checkout] = nil
     checkin = params[:date][:checkin_date]
     checkout = params[:date][:checkout_date]
-    reserved_dates = Reservation.dates_reserved(
-      params[:id], checkin, checkout
-    )
-    if Date.strptime(checkin, "%m/%d/%Y") > Date.strptime(checkout, "%m/%d/%Y")
-      flash[:error] = "You must select a checkin date before your checkout date"
-    elsif reserved_dates.empty?
-      @cart.add_home(params[:id], checkin, checkout)
-      session[:cart] = @cart.contents
-      flash[:success] = "You've added this reservation to your cart"
+    if checkin == "" || checkout == ""
+      flash[:error] = "You must have a checkin and checkout date"
     else
-      flash[:error] = "This home is already reserved on #{reserved_dates.join(", ")}"
+      reserved_dates = Reservation.dates_reserved(
+        params[:id], checkin, checkout
+      )
+      if Date.strptime(checkin, "%m/%d/%Y") > Date.strptime(checkout, "%m/%d/%Y")
+        flash[:error] = "You must select a checkin date before your checkout date"
+      elsif reserved_dates.empty?
+        @cart.add_home(params[:id], checkin, checkout)
+        session[:cart] = @cart.contents
+        flash[:success] = "You've added this reservation to your cart"
+      else
+        flash[:error] = "This home is already reserved on #{reserved_dates.join(", ")}"
+      end
     end
     redirect_to request.referrer
   end
